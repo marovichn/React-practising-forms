6b763df09287696516e3d683c43ae9c3dd89e48a
marovichn <nikolamarovic.11@gmail.com>
2023-04-01 21:50:07 +0200

    using-useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,22 +1,23 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
 
-  const onChangeHandler = (e) => {
+  /*  const onChangeHandler = (e) => {
     setEnteredName(e.target.value);
-  };
+  }; */
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    console.log(enteredName);
+    setEnteredName(nameInputRef.current.value);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input onChange={onChangeHandler} type="text" id="name" />
+        <input ref={nameInputRef} type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
